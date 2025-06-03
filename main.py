1f79b2fd9e537c21cb6cbf5944ed11de026b5acb
IAN <ingenieroiansuarez@gmail.com>
2025-06-03 16:29:42 -0500

    update web scraping script and enhance data visualization

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
+# This script captures video from the webcam, detects motion, and sends an email alert when motion is detected.
 import requests
 import selectorlib
 
 from datetime import datetime
 
-
+# This script scrapes a webpage for tour data, extracts the relevant information, and stores it in a text file with a timestamp.
 URL = "http://programmer100.pythonanywhere.com/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
+# # The URL to scrape for tour data
 def scrape(url):
     """Scrape the page source from the URL"""
     response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
 
-
+# This function extracts the tour data from the scraped HTML source using selectorlib.
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
 
-
+# This function stores the extracted data in a text file with a timestamp.
 def store(extracted):
     now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
     with open("data.txt", "a") as file:
--- webapp.py
+++ webapp.py
@@ -2,9 +2,9 @@ import streamlit as st
 import plotly.express as px
 import pandas
 
-
+# Load data from a text file
 df = pandas.read_csv("data.txt")
-
+# Convert the 'date' column to datetime format
 figure = px.line(x=df["date"], y=df["temperature"],
                              labels={"x": "Date", "y": "Temperature (C)"})
 
