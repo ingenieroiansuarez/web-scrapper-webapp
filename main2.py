a8d8e5b9eb9f4da80b4bdcbcfe7c4ad020805f7f
IAN <ingenieroiansuarez@gmail.com>
2025-06-03 20:40:06 -0500

    refactor web scraping scripts to store data in SQLite database

--- main2.py
+++ main2.py
@@ -1,33 +1,33 @@
+# This script captures video from the webcam, detects motion, and sends an email alert when motion is detected.
 import requests
 import selectorlib
 import sqlite3
 from datetime import datetime
 
-
+connection = sqlite3.connect("data1.db")
+# This script scrapes a webpage for tour data, extracts the relevant information, and stores it in a text file with a timestamp.
 URL = "http://programmer100.pythonanywhere.com/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-connection = sqlite3.connect("temperatures.db")
-
+# # The URL to scrape for tour data
 def scrape(url):
     """Scrape the page source from the URL"""
     response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
 
-
+# This function extracts the tour data from the scraped HTML source using selectorlib.
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
 
-
+# This function stores the extracted data in a text file with a timestamp.
 def store(extracted):
     now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO database (timestamp, tours) VALUES (?, ?)",
-                   (now, extracted))
-    connection.commit()
+    with open("data.txt", "a") as file:
+        line = f"{now},{extracted}\n"
+        file.write(line)
 
 
 if __name__ == "__main__":
--- webapp.py
+++ webapp.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import plotly.express as px
-import pandas
+import sqlite3
 
-# Load data from a text file
-df = pandas.read_csv("data.txt")
-# Convert the 'date' column to datetime format
-figure = px.line(x=df["date"], y=df["temperature"],
+connection = sqlite3.connect("temperatures.db")
+cursor = connection.cursor()
+cursor.execute("SELECT date FROM temperatures")
+date = cursor.fetchall()
+date = [item[0] for item in date]
+
+cursor.execute("SELECT temperature FROM temperatures")
+temperatures = cursor.fetchall()
+temperatures = [item[0] for item in temperatures]
+
+
+figure = px.line(x=date, y=temperatures,
                              labels={"x": "Date", "y": "Temperature (C)"})
 
 st.plotly_chart(figure)
